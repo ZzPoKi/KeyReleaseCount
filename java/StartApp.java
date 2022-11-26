8e05b7ab88a85427e9c6822e306d2eaa8e184719
ZzPoKi0_0 <92191993+ZzPoKi@users.noreply.github.com>
2022-11-26 23:01:05 +0800

    Update StartApp.java

--- java/StartApp.java
+++ java/StartApp.java
@@ -1,8 +1,9 @@
 package Test1;
-
+//程序入口
 public class StartApp {
     public static void main(String[] args) {
-        new StartGame();
+        new KeyPressCount();
+        //new StartGame();
         //GlobalScreen.addNativeKeyListener(new KeyPressedCount());
     }
 }
